e0b7bbe1c239635060cdb65954ae72d58de50ca5
e3fager <christian.fager@chalmers.se>
2012-01-30 11:44:48 +0100

    Test

--- @mna/private/calc_dz.m
+++ @mna/private/calc_dz.m
@@ -1,9 +1,9 @@
 function dz = calc_dz(Va,V,x,paramlist)
 %CALC_DZ Calculates Z-sensitivities given branch voltages
 % $Header$
-% $Author: koffer $
-% $Date: 2005-09-12 16:26:57 +0200 (Mon, 12 Sep 2005) $
-% $Revision: 297 $ 
+% $Author: koffe $
+% $Date: 2009-01-13 11:05:46 +0100 (ti, 13 jan 2009) $
+% $Revision: 96 $ 
 % $Log$
 % Revision 1.6  2005/09/12 14:26:57  koffer
 % *** empty log message ***
